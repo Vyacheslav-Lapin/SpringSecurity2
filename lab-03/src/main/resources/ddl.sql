986aab117c42abd9f66f68446d65284aca9e5838
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-07-12 17:04:14 +0300

    configs

--- lab-03/src/main/resources/ddl.sql
+++ lab-03/src/main/resources/ddl.sql
@@ -1,3 +1,11 @@
+CREATE TABLE contact (
+  id                INT AUTO_INCREMENT,
+  contact_name      VARCHAR(50),
+  contact_telnumber VARCHAR(20),
+
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE users (
   username VARCHAR(50) NOT NULL PRIMARY KEY,
   password VARCHAR(50) NOT NULL,
