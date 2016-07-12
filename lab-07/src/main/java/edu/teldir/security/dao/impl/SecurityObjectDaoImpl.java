98d8a8a69225b800589d904c4d48bd89422e15ec
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-07-12 16:36:55 +0300

    Compiled

--- lab-07/src/main/java/edu/teldir/security/dao/impl/SecurityObjectDaoImpl.java
+++ lab-07/src/main/java/edu/teldir/security/dao/impl/SecurityObjectDaoImpl.java
@@ -2,7 +2,7 @@ package edu.teldir.security.dao.impl;
 
 import edu.teldir.security.dao.SecurityObjectDao;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,14 +12,14 @@ import java.util.Collection;
  * @author Anton German &lt;AGerman@epam.com&gt;
  * @version 1.0 15.04.12
  */
-public class SecurityObjectDaoImpl extends SimpleJdbcDaoSupport implements SecurityObjectDao {
+public class SecurityObjectDaoImpl extends JdbcDaoSupport implements SecurityObjectDao {
     public Collection<String> getRoles() {
-        return getSimpleJdbcTemplate()
+        return getJdbcTemplate()
                 .query("select distinct authority from authorities order by authority", new StringRowMapper());
     }
 
     public Collection<String> getUsernames() {
-        return getSimpleJdbcTemplate()
+        return getJdbcTemplate()
                 .query("select username from users where enabled = 1 order by username", new StringRowMapper());
     }
 
